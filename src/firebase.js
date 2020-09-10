38a04b5673ff5016af1596983ebdd8bbb7a3eb20
pogadev <pogadev18@gmail.com>
2020-09-10 10:24:05 +0300

    react boilerplate

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyDMrCauXWox_kCeueDIusw9oYGcw6dys1Q',
+  authDomain: 'clone-60d22.firebaseapp.com',
+  databaseURL: 'https://clone-60d22.firebaseio.com',
+  projectId: 'clone-60d22',
+  storageBucket: 'clone-60d22.appspot.com',
+  messagingSenderId: '961379622188',
+  appId: '1:961379622188:web:68a22dc29a65563ae26089',
+  measurementId: 'G-XH909VD6FN'
+};
